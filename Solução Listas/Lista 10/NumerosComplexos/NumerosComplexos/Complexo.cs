ab456a0eb06402ebfb0be72520d10daeca79262f
Ítalo Berg Marques Souza dos Reis <italoberg@yahoo.com.br>
2015-01-28 16:38:22 -0200

    Lista10
    C# e WPF
    Desktop Windows Application

---
+++ Solução Listas/Lista 10/NumerosComplexos/NumerosComplexos/Complexo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosComplexos
+{
+    public class Complexo
+    {
+        private double real;
+        private double imag;
+
+        public Complexo()
+        {
+
+        }
+
+        public Complexo(double real, double imag)  
+        {
+          this.real = real;
+          this.imag = imag;
+        }
+
+        public double Real
+        {
+            set { real = value; }
+            get { return real;  }
+        }
+
+        public double Imag
+        {
+            set { imag = value; }
+            get { return imag;  }
+        }
+
+        public static Complexo operator +(Complexo c1, Complexo c2) 
+        {
+            return new Complexo(c1.Real + c2.Real, c1.Imag + c2.Imag);
+        }
+
+        public static Complexo operator -(Complexo c1, Complexo c2)
+        {
+            return new Complexo(c1.Real - c2.Real, c1.Imag - c2.Imag);
+        }
+
+        public static Complexo operator /(Complexo c1, Complexo c2)
+        {
+            double r = (c1.real * c2.real + c1.imag * c2.imag) / (c2.real * c2.real + c2.imag * c2.imag);
+            double i = (c2.real * c1.imag - c1.real * c2.imag) / (c2.real * c2.real + c2.imag * c2.imag);
+
+            return new Complexo(r,i);
+        }
+
+        public static Complexo operator *(Complexo c1, Complexo c2)
+        {
+            double r = (c1.real * c2.real + c1.imag * c2.imag) / (c2.real * c2.real + c2.imag * c2.imag);
+            double i = (c2.real * c1.imag - c1.real * c2.imag) / (c2.real * c2.real + c2.imag * c2.imag);
+
+            return new Complexo(r, i);
+        }
+
+        
+        public override string ToString()
+       {
+         return (System.String.Format("{0} + {1}i", real, imag));
+       }
+
+
+
+    }
+}
---
+++ Solução Listas/Lista 10/NumerosComplexos/NumerosComplexos/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumerosComplexos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public double xr;
+        public double xi;
+        public double yr;
+        public double yi;
+        Complexo c1;
+        Complexo c2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Complexo c1 = new Complexo();
+            Complexo c2 = new Complexo();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            xr = Double.Parse(TBXReal.Text);
+            xi = Double.Parse(TBXImag.Text);
+            c1 = new Complexo(xr, xi);
+
+            yr = Double.Parse(TBYReal.Text);
+            yi = Double.Parse(TBYImag.Text);
+            c2 = new Complexo(yr, yi);
+            TBResultado.Text = (c1 + c2).ToString();
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            xr = Double.Parse(TBXReal.Text);
+            xi = Double.Parse(TBXImag.Text);
+            c1 = new Complexo(xr, xi);
+
+            yr = Double.Parse(TBYReal.Text);
+            yi = Double.Parse(TBYImag.Text);
+            c2 = new Complexo(yr, yi);
+            TBResultado.Text = (c1 - c2).ToString();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            xr = Double.Parse(TBXReal.Text);
+            xi = Double.Parse(TBXImag.Text);
+            c1 = new Complexo(xr, xi);
+
+            yr = Double.Parse(TBYReal.Text);
+            yi = Double.Parse(TBYImag.Text);
+            c2 = new Complexo(yr, yi);
+            TBResultado.Text = (c1 * c2).ToString();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            xr = Double.Parse(TBXReal.Text);
+            xi = Double.Parse(TBXImag.Text);
+            c1 = new Complexo(xr, xi);
+
+            yr = Double.Parse(TBYReal.Text);
+            yi = Double.Parse(TBYImag.Text);
+            c2 = new Complexo(yr, yi);
+            TBResultado.Text = (c1 / c2).ToString();
+        }
+    }
+}
