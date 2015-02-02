044eb9ce5d5dc4224d49a678e002590f4707ad0e
Ítalo Berg Marques Souza dos Reis <italoberg@yahoo.com.br>
2015-02-02 17:09:38 -0200

    Questão 1 da Lista 10 Adicionada
    Fibonacci

---
+++ Solução Listas/Lista 10/Fibonacci/Fibonacci/Fib.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class Fib
+    {
+        public int a1;
+        public int a2;
+        public int next;
+        public int n;
+        public Fib()
+        {
+            a1 = 1;
+            a2 = 1;
+            next = a1 + a2;
+            n = 1;
+        }
+
+        public void Reiniciar()
+        {
+            a1 = 1;
+            a2 = 1;
+            next = a1 + a2;
+        }
+
+        public int Proximo()
+        {
+            int saida =1;
+
+            if (n == 1 || n == 2)
+            {
+                next = 1;
+                n++;
+            }
+
+            else
+            {
+                next = a1 + a2;
+                a1 = a2;
+                a2 = next;
+            }
+            return next;
+        }
+    }
+}
---
+++ Solução Listas/Lista 10/Fibonacci/Fibonacci/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fibonacci
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Fib f = new Fib();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ButtonRest(object sender, RoutedEventArgs e)
+        {
+            f.Reiniciar();
+        }
+
+        private void ButtonNext(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text += f.Proximo().ToString() + System.Environment.NewLine;
+        }
+    }
+}
